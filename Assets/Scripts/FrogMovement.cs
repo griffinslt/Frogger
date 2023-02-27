7092d4005d19cf567d96d73fb6a66928b7ad7aa0
griffinslt <slt.griffin3105@gmail.com>
2023-02-27 11:24:17 +0000

    Files moved

---
+++ Assets/Scripts/FrogMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FrogMovement : MonoBehaviour
+{
+
+
+    [SerializeField] private float speed;
+    Rigidbody2D rb2D;
+    private Vector2 currentPos;
+
+    private void Awake()
+    {
+        print("awake player");
+        rb2D = GetComponent<Rigidbody2D>();
+        currentPos = transform.position;
+        print(currentPos);
+        
+    }
+
+    private void Update()
+    {
+        checkMovement();
+        
+    }
+
+    private void checkMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
+            transform.eulerAngles = new Vector3(0,0,0);
+            transform.position = currentPos + new Vector2(0,1);
+            currentPos += new Vector2(0,1);
+        } else if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            transform.eulerAngles = new Vector3(0,0,180);
+            transform.position = currentPos + new Vector2(0,-1);
+            currentPos += new Vector2(0,-1);
+        } else if (Input.GetKeyDown(KeyCode.D)) 
+        {
+            transform.eulerAngles = new Vector3(0,0,-90);
+            transform.position = currentPos + new Vector2(1,0);
+            currentPos += new Vector2(1, 0);
+        } else if (Input.GetKeyDown(KeyCode.A)) 
+        {
+            transform.eulerAngles = new Vector3(0,0,90);            
+            transform.position = currentPos + new Vector2(-1,0);
+            currentPos += new Vector2(-1, 0);
+            
+        }
+
+    }
+
+    
+}
---
+++ Assets/Scripts/LogMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum StartingSide
+{
+    LEFT = 0,
+    RIGHT = 1,
+}
+
+public class LogMovement : MonoBehaviour
+{
+
+    // [SerializableField] private float speed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
