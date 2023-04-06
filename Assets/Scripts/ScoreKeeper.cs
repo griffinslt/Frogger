8693ccb4e4051d4f61e4ee97f9d3d986152ad3b3
griffinslt <slt.griffin3105@gmail.com>
2023-04-06 19:17:24 +0100

    ToJson for score data

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class ScoreKeeper : MonoBehaviour
@@ -9,6 +10,13 @@ public class ScoreKeeper : MonoBehaviour
     private int _scoreMultiplier = 1; //change this when using a 2x multiplier or something
     private int _score;
     public TextMeshProUGUI scoreMessage;
+
+    private struct ScoreData
+    {
+        public int ScoreMultiplier;
+        public int Score;
+        public string ScoreMessage;
+    }
 
     public void AddScore(int amount)
     {
@@ -22,6 +30,15 @@ public class ScoreKeeper : MonoBehaviour
     {
         return _score;
     }
-    
-    
+
+    public string ToJson()
+    {
+        var data = new ScoreData()
+        {
+            ScoreMultiplier = _scoreMultiplier,
+            Score = _score,
+            ScoreMessage = scoreMessage.text,
+        };
+        return JsonUtility.ToJson(data);
+    }
 }
