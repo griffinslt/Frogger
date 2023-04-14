2f114cd213d994268d3549f3cf7114d7d6a0e4cc
griffinslt <slt.griffin3105@gmail.com>
2023-04-14 11:38:28 +0100

    New Button Loads into level 1

---
+++ Assets/Scripts/ButtonBehaviours/NewGameButtonBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ButtonBehaviours
+{
+    public class NewGameButtonBehaviour : MonoBehaviour
+    {
+        public void OnButtonPress()
+        {
+            SceneManager.LoadScene("Level1"); 
+        }
+    }
+}
